29e14f5e85fe12100df93fac4748ab985a9563a6
Snowers <lucca.al@hotmail.com>
2023-09-26 22:33:40 -0300

    Handlebars

---
+++ 3_HANDLEBARS/2_layout/index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+// config handlebars com template engine
+
+app.engine('handlebars',exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) =>{
+    res.render('home', {layout: false})
+})
+
+app.listen(3000, () => {
+    console.log("servidor rodando na porta 'igor'3k!")
+})
