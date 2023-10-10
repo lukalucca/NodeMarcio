8c6721d5d9c3c7ca0410444bddfcdce709dad272
Lucca Alves <lucca.al@hotmail.com>
2023-10-09 23:21:54 -0300

    Condicionais no handlebars

--- 3_HANDLEBARS/4_condicionais/3_Dados_para_View/index.js
+++ 3_HANDLEBARS/4_condicionais/3_Dados_para_View/index.js
@@ -8,6 +8,10 @@ const app = express()
 app.engine('handlebars',exphbs.engine())
 app.set('view engine', 'handlebars')
 
+app.get('/dashboard',(req, res) =>{
+    res.render('dashboard')
+})
+
 app.get('/', (req, res) =>{
     const usuario = {
         nome:"Lucca",
@@ -15,10 +19,19 @@ app.get('/', (req, res) =>{
         idade:"25"
     }
 
+    const auth = false
 
-    res.render('home',{usuario})
+
+    res.render('home',{usuario, auth})
 })
 
 app.listen(3000, () => {
     console.log("servidor rodando na porta 'igor'3k!")
 })
+
+
+
+
+
+
+//Error: ENOENT: no such file or directory, open 'C:\Users\lukal\Desktop\NodeMode\3_HANDLEBARS\4_condicionais\3_Dados_para_View\views\layouts\main.handlebars'
